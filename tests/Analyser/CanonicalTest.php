4d85316a01b178d6d35107a1da5a4471c97162ea
Chris Shennan <chris@chrisshennan.com>
2018-02-24 21:14:10 +0000

    Adding sample test

---
+++ tests/Analyser/CanonicalTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Analyser;
+
+use PHPUnit\Framework\TestCase;
+
+class CanonicalTest extends TestCase
+{
+    public function testSample()
+    {
+        $this->assertTrue(1==1);
+    }
+}
